af954b56977cbc437467f3017895fc59b7b0f7a1
Andrew Morris <voltrevo@gmail.com>
2015-05-27 12:19:14 +1000

    Fixed linting issues in server/app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
-var static = require('node-static-alias');
+'use strict';
+
+var nodeStaticAlias = require('node-static-alias');
 var http = require('http');
 
 var args = require('minimist')(process.argv.slice(2));
@@ -7,16 +9,16 @@ var host = args.h || '0.0.0.0';
 var port = args.p || 8080;
 var socketPort = args.ws || 9001;
 
-var websockets = require('./websockets.js')(host, socketPort);
+require('./websockets.js')(host, socketPort);
 
-var file = new static.Server('webclient');
+var file = new nodeStaticAlias.Server('webclient');
 
-var app = http.createServer(function(req, res) {
+http.createServer(function(req, res) {
   console.log(req.url);
   req.addListener('end', function() {
     file.serve(req, res);
   }).resume();
 
 }).listen(port, host);
-console.log('Server running at http://' + host + ':' + port + '/');
 
+console.log('Server running at http://' + host + ':' + port + '/');
